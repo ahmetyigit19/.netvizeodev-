2862f7800dc7bb9f4b34d81f5eb378480c179b0d
ahmet yiğit uysal <75635382+ahmetyigit19@users.noreply.github.com>
2021-05-04 01:41:22 +0300

    a

---
+++ odevv/System/IO/file.cs
@@ -0,0 +1,15 @@
+﻿namespace System.IO
+{
+    internal class file
+    {
+        internal static bool exists(object hefef_yol)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void create(string hedef_yol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
